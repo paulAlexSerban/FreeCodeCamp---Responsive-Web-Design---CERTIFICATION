5ae738da05e200527599644fac30ca60a69cda73
Paul Serban <paul.alex.serban@gmail.com>
2021-07-20 22:41:12 +0300

    fcc-rwd_11_updated-tribute-page

--- src/pages/tribute_page/tribute_page.entry.js
+++ src/pages/tribute_page/tribute_page.entry.js
@@ -1,4 +1,12 @@
 require('normalize.css/normalize.css');
 import './tribute_page.styles.scss';
 
+// import STRUCTURE
+import headerStructure from '../../ui_ux/structure/header/header';
+import navigationStructure from '../../ui_ux/structure/navigation/navigation';
+
+// init STRUCTURE
+new headerStructure;
+new navigationStructure;
+
 console.log('hello tribute page');
